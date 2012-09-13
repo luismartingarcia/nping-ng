b91b6c00b50def093d892a5a274a7f3d0416cc67
luis <luis@e0a8ed71-7df4-0310-8962-fdc924857419>
2012-09-13 20:20:35 +0000

    Minor fix: set VB_3 instead of VB_0.
    git-svn-id: https://svn.nmap.org@29780 e0a8ed71-7df4-0310-8962-fdc924857419

--- nping/EchoClient.cc
+++ nping/EchoClient.cc
@@ -440,8 +440,8 @@ int EchoClient::nep_recv_echo(u8 *packet, size_t packetlen){
 
     /* @todo: compute the link layer offset from the DLT type and discard
      * link layer headers */
-    if( o.getVerbosity() >= VB_3)
-      print_hexdump(VB_0|NO_NEWLINE, pkt, pktlen);
+    if(o.getVerbosity() >= VB_3)
+      print_hexdump(VB_3|NO_NEWLINE, pkt, pktlen);
 
   }
 
