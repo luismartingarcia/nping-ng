9df70ac299d25b15b8d1e12f9fa8680169270516
Luis MartinGarcia <luis@luismg.com>
2015-04-06 01:29:20 +0200

    Use the new nsock_set_loglevel() function. This is a port of Henri's r30366 fix

--- nping/ProbeEngine.cc
+++ nping/ProbeEngine.cc
@@ -155,9 +155,9 @@ int ProbeEngine::init_nsock(){
     /* Set nsock trace level */
     gettimeofday(&now, NULL);
     if( o.getDebugging() == DBG_5)
-      nsp_settrace(nsp, NULL, 1 , &now);
+      nsock_set_loglevel(nsp, NSOCK_LOG_INFO);
     else if( o.getDebugging() > DBG_5 )
-      nsp_settrace(nsp, NULL, 10 , &now);
+      nsock_set_loglevel(nsp, NSOCK_LOG_DBG_ALL);
 
     /* If users passed an explicit network interface, set it in Nsock so
      * all IODs get bound to the interface. */
